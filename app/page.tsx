c5999ddfdfa178622479bfe38908c421ab0f1a9b
maylogger <vtuber@maylogger.com>
2024-09-28 17:38:09 +0800

    成功接上聊天

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import tmi from "tmi.js";
+
 export default function Home() {
+  const [messages, setMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const client = new tmi.Client({
+      channels: ["may_logger"],
+    });
+
+    client.connect();
+
+    client.on("message", (channel, tags, message, self) => {
+      setMessages((prev) => [
+        ...prev,
+        `${tags["display-name"]}: ${message} ${self}`,
+      ]);
+    });
+
+    return () => {
+      client.disconnect();
+    };
+  }, []);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Twitch 聊天室訊息</h1>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
     </div>
   );
 }
