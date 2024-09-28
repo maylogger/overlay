53f79d5332c1a7151caa073ec7db8c11c0859036
maylogger <vtuber@maylogger.com>
2024-09-29 01:33:01 +0800

    add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,78 @@
 
 import { useEffect, useState } from "react";
 import tmi from "tmi.js";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export default function Home() {
   const [messages, setMessages] = useState<string[]>([]);
+  const [channel, setChannel] = useState<string>("");
+
+  const formSchema = z.object({
+    channel: z.string().min(1, { message: "請輸入頻道名稱" }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      channel: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setChannel(values.channel);
+  }
 
   useEffect(() => {
+    if (!channel) return;
+
     const client = new tmi.Client({
-      channels: ["may_logger"],
+      channels: [channel],
     });
 
     client.connect();
 
     client.on("message", (channel, tags, message, self) => {
-      setMessages((prev) => [
-        ...prev,
-        `${tags["display-name"]}: ${message} ${self}`,
-      ]);
+      setMessages((prev) => [...prev, `${tags["display-name"]}: ${message}`]);
     });
 
     return () => {
       client.disconnect();
     };
-  }, []);
+  }, [channel]);
 
   return (
     <div>
-      <h1>Twitch 聊天室訊息</h1>
+      <h1>自製 Twitch 聊天室訊息</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="channel"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Twitch 頻道名稱</FormLabel>
+                <FormControl>
+                  <Input placeholder="輸入頻道名稱" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">連接</Button>
+        </form>
+      </Form>
       <ul>
         {messages.map((msg, index) => (
           <li key={index}>{msg}</li>
