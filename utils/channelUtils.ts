829a118c7a7ff507c363bf0eb509b371f77ed65c
maylogger <vtuber@maylogger.com>
2024-09-30 16:57:17 +0800

    Refactor channel and message utilities for improved Twitch emote handling

--- utils/channelUtils.ts
+++ utils/channelUtils.ts
@@ -1,6 +1,9 @@
-export function extractChannelName(input: string): string {
+export function extractChannelName(input: string) {
+  // 如果字串是 URL，就解析 URL
   const url = input.startsWith("http") ? new URL(input) : null;
+  // 如果 URL 是 twitch.tv，就回傳 channel name
+  // 如果 URL 不是 twitch.tv，就回傳整個字串
   return url?.hostname === "www.twitch.tv" || url?.hostname === "twitch.tv"
-    ? url.pathname.split("/").filter((segment) => segment !== "")[0] || input
+    ? url.pathname.split("/").filter((segment) => segment !== "")[0]
     : input;
 }
--- utils/messageUtils.ts
+++ utils/messageUtils.ts
@@ -1,14 +1,25 @@
-export function getMessageHTML(
-  message: string,
-  { emotes }: { emotes: Record<string, string[]> }
-): string {
+// 型別定義
+interface Emote {
+  [id: string]: string[];
+}
+interface StringReplacement {
+  stringToReplace: string;
+  replacement: string;
+}
+interface MessageWithEmotes {
+  message: string;
+  emotes: Emote;
+}
+
+// 修改函式簽名
+export function getMessageHTML({ message, emotes }: MessageWithEmotes) {
+  // 如果沒有表情符號，就直接回傳 message
   if (!emotes) return message;
 
-  const stringReplacements: {
-    stringToReplace: string;
-    replacement: string;
-  }[] = [];
+  // 用來存放 emote 替換的 string
+  const stringReplacements: StringReplacement[] = [];
 
+  // 遍歷 emotes，將 emote 替換為 img 標籤
   Object.entries(emotes).forEach(([id, positions]) => {
     const position = positions[0];
     const [start, end] = position.split("-");
@@ -16,13 +27,13 @@ export function getMessageHTML(
       parseInt(start, 10),
       parseInt(end, 10) + 1
     );
-
     stringReplacements.push({
       stringToReplace: stringToReplace,
       replacement: `<img src="https://static-cdn.jtvnw.net/emoticons/v2/${id}/default/dark/3.0">`,
     });
   });
 
+  // 將 message 中的 emote 替換為 img 標籤
   const messageHTML = stringReplacements.reduce(
     (acc, { stringToReplace, replacement }) => {
       return acc.split(stringToReplace).join(replacement);
