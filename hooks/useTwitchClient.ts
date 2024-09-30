604bade02b0860f113f853414dedb0f11a25cd9b
maylogger <vtuber@maylogger.com>
2024-09-30 18:31:18 +0800

    Implement color utility functions for enhancing chat message contrast

--- hooks/useTwitchClient.ts
+++ hooks/useTwitchClient.ts
@@ -16,6 +16,45 @@ export function useTwitchClient(channel: string) {
 
     client.connect().then(() => setIsConnected(true));
 
+    const defaultColors = [
+      "#FF0000",
+      "#0000FF",
+      "#008000",
+      "#B22222",
+      "#FF7F50",
+      "#9ACD32",
+      "#FF4500",
+      "#2E8B57",
+      "#DAA520",
+      "#D2691E",
+      "#5F9EA0",
+      "#1E90FF",
+      "#FF69B4",
+      "#8A2BE2",
+      "#00FF7F",
+    ];
+    const randomColorsChosen: Record<string, Record<string, string>> = {};
+
+    function resolveColor(
+      channel: string,
+      name: string,
+      color: string | undefined
+    ) {
+      if (color !== null) {
+        return color;
+      }
+      if (!(channel in randomColorsChosen)) {
+        randomColorsChosen[channel] = {};
+      }
+      if (name in randomColorsChosen[channel]) {
+        color = randomColorsChosen[channel][name];
+      } else {
+        color = defaultColors[Math.floor(Math.random() * defaultColors.length)];
+        randomColorsChosen[channel][name] = color;
+      }
+      return color;
+    }
+
     client.on("message", (channel, tags, message, self) => {
       if (self) return;
       setMessages((prev) => {
@@ -30,7 +69,11 @@ export function useTwitchClient(channel: string) {
             ? parseInt(tags["tmi-sent-ts"])
             : Date.now(),
           badges: tags.badges ? Object.keys(tags.badges) : undefined,
-          color: tags.color ? adjustColorForContrast(tags.color) : "#000",
+          color: resolveColor(
+            channel,
+            tags["display-name"] || tags["username"] || "未知用戶",
+            tags.color
+          ),
         };
         return [...prev, newMessage].slice(-10);
       });
